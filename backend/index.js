920ee208fc92c76dbd9653ebc958f40af63fcf22
Lucks21 <manobazete21@gmail.com>
2024-08-23 13:18:26 -0400

    inicio

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
